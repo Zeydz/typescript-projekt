d4a5ac9fec3e1a7598ede5e2a11067c161319957
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-05-22 14:40:58 +0200

    Börjat på startsida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CoursesComponent } from './courses/courses.component';
 import { OverallPlanComponent } from './overall-plan/overall-plan.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
     { path: 'courses', component: CoursesComponent},
     { path: 'overall-plan', component: OverallPlanComponent},
     { path: '', redirectTo: '/courses', pathMatch: 'full'},
